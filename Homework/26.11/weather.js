be744e1d574d352e1d404084bf042857d517ce70
IMT-Teilnehmer <kuzhnersasha@gmail.com>
2024-12-14 23:09:49 +0100

    14.12

--- Homework/26.11/weather.js
+++ Homework/26.11/weather.js
@@ -1,21 +1,42 @@
-const weatherDiv = document.getElementById('weatherCont');
-const weatherButton = document.getElementById('weatherBtn');
+// const weatherDiv = document.getElementById('weatherCont');
+// const weatherButton = document.getElementById('weatherBtn');
+// 
+// function getWeather(city = 'Eberbach') {
+    // fetch(`https://wttr.in/${city}?format=3`) 
+        // .then(response => response.text())   
+        // .then(data => {
+            // weatherDiv.textContent = data;   
+        // })
+        // .catch(error => {
+            // weatherDiv.textContent = 'Ошибка: не удалось получить данные.';
+        // });
+// }
+// 
+// weatherButton.addEventListener('click', () => {
+    // const city = prompt('Введите город:');
+    // getWeather(city);
+// });
+// 
+// 
+// getWeather();
 
-function getWeather(city = 'Eberbach') {
-    fetch(`https://wttr.in/${city}?format=3`) 
-        .then(response => response.text())   
-        .then(data => {
-            weatherDiv.textContent = data;   
-        })
-        .catch(error => {
-            weatherDiv.textContent = 'Ошибка: не удалось получить данные.';
+const button = document.getElementById("refreshButton");
+
+const CityUrl = "https://wttr.in/";
+const cityParams = {
+    2: "",
+    m: "",
+    lang: "ru",
+};
+
+function fetchData() {
+    const queryString = new URLSearchParams(cityParams).toString();
+    fetch(`${CityUrl}?${queryString}`)
+        .then((response) => response.text())
+        .then((pageText) => {
+            const result = document.getElementById("content");
+            result.innerHTML = pageText;
         });
 }
 
-weatherButton.addEventListener('click', () => {
-    const city = prompt('Введите город:');
-    getWeather(city);
-});
-
-
-getWeather();
+button.addEventListener("click", fetchData);
